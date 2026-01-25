2caec56905174c578ad0dafcf41f49f01de12ea2
אלעזר גרשי <emg4706@gmail.com>
2026-01-25 16:02:58 +0200

    creating phase 2

--- satellite.py
+++ satellite.py
@@ -1,5 +1,18 @@
-from space_network_lib import SpaceEntity, Packet, SpaceNetwork
+from space_network_lib import (DataCorruptedError,SpaceEntity,
+Packet, SpaceNetwork,TemporalInterferenceError)
+import time
 
+def attempt_transmission(packet :Packet):
+    sent = False
+    while not sent:
+        try:
+            my_space.send(packet)
+            sent = True
+        except TemporalInterferenceError:
+            print("Interference, waiting...")
+            time.sleep(2)
+        except DataCorruptedError:
+            print("Data corrupted, retrying...")
 
 class Satellite(SpaceEntity):
     def __init__(self,name,distance_from_earth):
@@ -8,9 +21,10 @@ class Satellite(SpaceEntity):
     def receive_signal(self, packet: Packet):
         print(f"Satellite : {self.name}\nReceived {packet}")
 
-my_space = SpaceNetwork()
+my_space = SpaceNetwork(level=2)
 sat1 = Satellite("Sat1",100)
 sat2 = Satellite("Sat2",200)
 
 p1 = Packet("Hello, How are you?",sat1,sat2)
-my_space.send(p1)
+
+attempt_transmission(p1)
