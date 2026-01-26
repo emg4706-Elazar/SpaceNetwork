5bb33d40b8a6e1a75c5d572903e2f4ece9907609
אלעזר גרשי <emg4706@gmail.com>
2026-01-26 11:54:12 +0200

    creating level 4

--- satellite.py
+++ satellite.py
@@ -7,9 +7,24 @@ import time
 class BrokenConnectionError(Exception):
     pass
 
+class RelayPacket(Packet):
+    def __init__(self,packet_to_relay, sender, proxy):
+        super().__init__(packet_to_relay,sender,proxy)
+
+    def __repr__(self):
+        return f"RelayPacket(Relaying [{self.data}] to {self.receiver}from {self.sender})"
+
+class SpaceEntityNotSat(SpaceEntity):
+    def __init__(self, name, distance_from_earth):
+        super().__init__(name, distance_from_earth)
+
+    def receive_signal(self, packet: Packet):
+        pass
+
 
 def attempt_transmission(packet :Packet):
     sent = False
+    print(f"from {packet.sender.name}")
     while not sent:
         try:
             my_space.send(packet)
@@ -30,15 +45,24 @@ class Satellite(SpaceEntity):
         super().__init__(name, distance_from_earth)
 
     def receive_signal(self, packet: Packet):
-        print(f"Satellite : {self.name}\nReceived {packet}")
+        if isinstance(packet,RelayPacket):
+            inner_packet = packet.data
+            print(f"Unwrapping and forwarding to {inner_packet.receiver}")
+            attempt_transmission(inner_packet)
+        else:
+            print(f"Final destination reached: {packet.data}" )
 
 my_space = SpaceNetwork(level=3)
+earth = SpaceEntityNotSat("Earth",0)
 sat1 = Satellite("Sat1",100)
 sat2 = Satellite("Sat2",200)
 
 p1 = Packet("Hello, How are you?",sat1,sat2)
+p_final = Packet("Hello from Earth",sat1,sat2)
+p_earth_to_sat1 = RelayPacket(p_final,earth,sat1)
 
 try:
-    attempt_transmission(p1)
+    attempt_transmission(p_earth_to_sat1)
 except:
     print("Transmission failed")
+
